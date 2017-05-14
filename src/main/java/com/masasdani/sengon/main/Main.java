00202790f077bc7e2bb2a0bd274d68bf25ceeae6
Afiq <afiqrasyidm@gmail.com>
2017-05-14 22:20:03 +0700

    Update Main.java
    edit

--- src/main/java/com/masasdani/sengon/main/Main.java
+++ src/main/java/com/masasdani/sengon/main/Main.java
@@ -10,13 +10,11 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 import com.masasdani.sengon.SentimentAnalysis;
-
 import jsastrawi.morphology.DefaultLemmatizer;
 import jsastrawi.morphology.Lemmatizer;
 
 public class Main {
-	public static void main(String args[]) throws IOException{
-		
+	public static void main(String args[]) throws IOException {
 		Set<String> dictionary = new HashSet<String>();
 		InputStream in = Lemmatizer.class.getResourceAsStream("/root-words.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -27,44 +25,56 @@ public class Main {
 		}
 
 		Lemmatizer lemmatizer = new DefaultLemmatizer(dictionary);
-		// Selesai setup JSastrawi
-		// lemmatizer bisa digunakan berkali-kali
-		ArrayList<String> kalimat = new ArrayList<String>();
-		kalimat.add("Dia Benci Kamu");
-		kalimat.add("Aku Menyumbang");
-		StringTokenizer tokens;
-		for(int i=0; i<kalimat.size(); i++){
-			tokens = new StringTokenizer(kalimat.get(i));
+		String kalimat = "";
+		StringTokenizer tokens = new StringTokenizer(kalimat);
+		ArrayList<String> isi = new ArrayList<>();
+		ArrayList<String> nama = new ArrayList<>();
+		int iii = -1;
+		while (tokens.hasMoreTokens()) {
+			String cek = tokens.nextToken();
+			String namaSubjek = "";
+			boolean stillNamaSubjek = false;
+			if (cek.charAt(cek.length() - 1) == '[') {
+				nama.add(cek.substring(0,cek.length()-2));
+				isi.add("");
+				iii++;
+			
+			} else {
+				if (cek.equals("]") || cek.equals("],")) {
+						
+				} else {
+					isi.set(iii, isi.get(iii) + " " + cek);
+				}
+
+			}
+
+		}
+
+		
+		for(int i =0; i<isi.size(); i++){
+			tokens = new StringTokenizer(isi.get(i));
 			String contoh1 = "";
 			String space = " ";
 			while(tokens.hasMoreTokens()){
-				if(tokens.countTokens()==1){	
+				if(tokens.countTokens()==1){
+					
 					contoh1 = contoh1 + lemmatizer.lemmatize(tokens.nextToken());
+					//System.out.println(i);
+
 				} else{
 					contoh1 = contoh1 + lemmatizer.lemmatize(tokens.nextToken()) + space;
 				}	
 			}
-			kalimat.set(i, contoh1);
+			isi.set(i, contoh1);
 		}
 		
 		
-		for(int i = 0; i<kalimat.size(); i++){
-//			System.out.println(kalimat.get(i));
-			String arguments1[] = {"-l", "id", "-t", kalimat.get(i)};
+		for(int i = 0; i<isi.size(); i++){
+			System.out.println(nama.get(i));
+			String arguments1[] = {"-l", "id", "-t", isi.get(i)};
 			new SentimentAnalysis().run(arguments1);
 		}
-//		System.out.println(lemmatizer.lemmatize("memakan"));
-//		System.out.println(lemmatizer.lemmatize("meminum"));
-//		System.out.println(lemmatizer.lemmatize("bernyanyi"));
-
-
-		// Always wrap FileReader in BufferedReader.
-		// Always close files.
 		
-//		String arguments1[] = {"-l", "id", "-t", "kejahatan"};
-//		new SentimentAnalysis().run(arguments1);
-
 	}
-	
-	
+
 }
